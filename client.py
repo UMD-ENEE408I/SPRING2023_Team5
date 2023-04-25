64744530bdf9686b5131ba72736c870a7ccddf0e
ryansipa <70705912+ryansipa@users.noreply.github.com>
2023-04-25 11:44:59 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -1,35 +1,81 @@
 import socket 
+import struct
+import threading
+import os
+import time
+import numpy as np
 
-localIP = "192.168.2.10"
+localIP = ''
 localPort = 3333
+localAny = "0.0.0.0"
 
-mice1_address = 1111
-mice2_address = 2222
-highIntensity1 = str.encode("Value of Intensity")
-highIntensity2 = str.encode("Value of intensity")
+global m1_address
+global m2_address
+global m1_port
+global m2_port
+global m1_theta
+global m2_theta
+global m1_en
+global m2_en
 
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-UDPServerSocket.bind((localIP, localPort))
+m1_theta = 5
+m2_theta = 10
+
+
+msg = "10"
+
+UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) #Creates UDP socket
+UDPServerSocket.bind((localAny, localPort))
+
 print("UDP server up and listening")
-while(True):
-    # If condition true, start receiving data
-    #x = UDPServerSocket.recvfrom(xSize)
-    #y = UDPServerSocket.recvfrom(ySize)
-    #z = UDPServerSocket.recvfrom(zSize)
-    #x_coordinate = x[0]
-    # x_address = x[1]
-    #y_coordinate = y[0]
-    # x_address = y[1]
-    #z_coordinate = z[0]
-    # x_address = z[1]
-    #address = bytesAddressPair[1]
-    #clientMsg = "X Coordinate:{}".format(x_coordinate)
-    #print(clientMsg)
-    #clientMsg = "Y Coordinate:{}".format(y_coordinate)
-    #print(clientMsg)
-    #clientMsg = "Z Coordinate:{}".format(z_coordinate)
-    #print(clientMsg)
 
-    # Do computation to calculate V, theta, etc and send data back to robots motor
-    UDPServerSocket.sendto(highIntensity1, mice1_address) #<--- Sends Data to mice 1
-    UDPServerSocket.sendto(highIntensity2, mice2_address) #<--- Sends Data to mice 2
+def get_address():
+    counter = 0
+    while counter < 1:
+        (mice, address) = UDPServerSocket.recvfrom(1024)
+        if mice.decode() == "mice1":
+            m1_address = address[0]
+            m1_port = address[1]
+            #print('Mice 1 address: {}'.format(mice.decode()))
+            UDPServerSocket.sendto(msg.encode(), (m1_address, m1_port))
+            counter = counter + 1
+            print('Mice 1 Connected!!!')
+
+        elif mice.decode() == "mice2":
+            m2_address = address[0]
+            m2_port = address[1]
+            UDPServerSocket.sendto(msg.encode(), (m2_address, m2_port))
+            counter = counter + 1
+            print('Mice 2 Connected!!!')
+
+
+def send():
+    while True:
+        if m1_parity == 1: #After computing the theta, enable the condition and send theta value
+            UDPServerSocket.sendto(m1_theta.encode(), m1_address)
+            m1_parity = 0
+        elif m2_parity == 1:
+            UDPServerSocket.sendto(m2_theta.encode(), m2_address)
+            m2_parity = 0
+
+#def receive():
+    #global m1_parity
+    #global m2_parity
+    while True:
+        msg1 = UDPServerSocket.recvfrom(1024)
+        if msg1[1] == m1_address:
+            print(msg1[0].decode() + "\n")
+            m1_parity = 1
+        elif msg1[1] == m1_address:
+            print(msg1[0].decode() + "\n")
+            m2_parity = 1
+
+
+x1 = threading.Thread(target = get_address)
+#x2 = threading.Thread(target = send)
+#x3 = threading.Thread(target = receive)
+
+x1.start()
+#time.sleep(3)
+#x2.start()
+#x3.start()
